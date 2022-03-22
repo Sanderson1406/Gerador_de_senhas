7d38bec783e944ccb7342e33b95db996cc7cb03a
Sanderson1406 <sandersonoficial10@hotmail.com>
2022-03-22 20:30:40 +0000

    Enter

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Teste")
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-input")
+let charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
