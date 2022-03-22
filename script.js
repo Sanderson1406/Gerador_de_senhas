75ecb49373949ea4adbbe34fadc0ceba616a12c4
Sanderson1406 <sandersonoficial10@hotmail.com>
2022-03-22 20:42:11 +0000

    Enter

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
+
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
-let containerPassword = document.querySelector("#container-input")
-let charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+let containerPassword = document.querySelector("#container-input");
+
+let charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
